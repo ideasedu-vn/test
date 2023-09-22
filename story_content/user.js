af9744e8befc1f0358bf6e1b0d20296dbf99da6a
trangpt226 <82557338+trangpt226@users.noreply.github.com>
2023-09-22 15:08:28 +0700

    Up

---
+++ story_content/user.js
@@ -0,0 +1,809 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6di08AmrXb0":
+        Script1();
+        break;
+      case "6EFfqu1g82d":
+        Script2();
+        break;
+      case "5kh1kAcj6hc":
+        Script3();
+        break;
+      case "5ZsieaYZC5y":
+        Script4();
+        break;
+      case "68CBDG28Mmj":
+        Script5();
+        break;
+      case "5lcviFggcqu":
+        Script6();
+        break;
+      case "5YbX94ye4cZ":
+        Script7();
+        break;
+      case "6SJ9xL7MblT":
+        Script8();
+        break;
+      case "6Vt6MKIf9bS":
+        Script9();
+        break;
+      case "5ycLYpzob2d":
+        Script10();
+        break;
+      case "6MQgHdLoe9f":
+        Script11();
+        break;
+      case "5Xvou0S5mBI":
+        Script12();
+        break;
+      case "5ZuZZuY0sdS":
+        Script13();
+        break;
+      case "6AREIl81ebp":
+        Script14();
+        break;
+      case "6Kw91hmmAlT":
+        Script15();
+        break;
+      case "6JM4JBIH2R1":
+        Script16();
+        break;
+      case "5sGknmGCWlY":
+        Script17();
+        break;
+      case "5XRdzz8EWfV":
+        Script18();
+        break;
+      case "6KST6tab9ob":
+        Script19();
+        break;
+      case "6mEGX9dwYOR":
+        Script20();
+        break;
+      case "60ob2B5gQpZ":
+        Script21();
+        break;
+      case "5VHb5pOnDxo":
+        Script22();
+        break;
+      case "5ZYRhA6aB0r":
+        Script23();
+        break;
+      case "5rQDeJy0BV6":
+        Script24();
+        break;
+      case "5hS3VZzOgu4":
+        Script25();
+        break;
+      case "6U93cJ3zWhX":
+        Script26();
+        break;
+      case "6o2BmcLLE68":
+        Script27();
+        break;
+      case "6WETQNPjrML":
+        Script28();
+        break;
+      case "6lIKVcoBgPe":
+        Script29();
+        break;
+      case "6bMLMoTlVbH":
+        Script30();
+        break;
+      case "5Yp5IcMmSrz":
+        Script31();
+        break;
+      case "5lsbBsvHqEy":
+        Script32();
+        break;
+      case "6glA1swEJU5":
+        Script33();
+        break;
+      case "6XTcimM0cY3":
+        Script34();
+        break;
+      case "6P1QBYiYdEl":
+        Script35();
+        break;
+      case "6bgzM1TPA0h":
+        Script36();
+        break;
+      case "5kVa1qKxafe":
+        Script37();
+        break;
+      case "5w0f7vT3z1n":
+        Script38();
+        break;
+      case "6NVGHnf7UiC":
+        Script39();
+        break;
+      case "6HiGGwlw9Qd":
+        Script40();
+        break;
+      case "6WqfjnpsjiZ":
+        Script41();
+        break;
+      case "6p3U5N048Xh":
+        Script42();
+        break;
+      case "5W62RvwfqZM":
+        Script43();
+        break;
+      case "5kpHkeOTyGu":
+        Script44();
+        break;
+      case "5fbygWXsTRj":
+        Script45();
+        break;
+      case "5hYmUNoKnvj":
+        Script46();
+        break;
+      case "6U63aBclqGv":
+        Script47();
+        break;
+      case "6pmvzGF5Vez":
+        Script48();
+        break;
+      case "6OkKP1gP5ET":
+        Script49();
+        break;
+      case "6Am2q4cKecL":
+        Script50();
+        break;
+      case "6BOoh8EVNwl":
+        Script51();
+        break;
+      case "5tyyxcHXs0I":
+        Script52();
+        break;
+      case "6MSVSXXWgE4":
+        Script53();
+        break;
+      case "6coONs1rjrH":
+        Script54();
+        break;
+      case "6J5Qg3R3AcP":
+        Script55();
+        break;
+      case "5evcFyjAdJp":
+        Script56();
+        break;
+      case "5wQkzotT4KR":
+        Script57();
+        break;
+      case "6I0inqbO46J":
+        Script58();
+        break;
+      case "5fukx74ifme":
+        Script59();
+        break;
+      case "6GDn104G6Dl":
+        Script60();
+        break;
+      case "6LfmTBpM8lZ":
+        Script61();
+        break;
+      case "6luiZqso2JF":
+        Script62();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+var e1 = player.GetVar('o3_a');
+var e2 = e1.replace(/\s+/g, ''); 
+player.SetVar('o3_a',e2);
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "1";
+player.SetVar('o'+inp, numb);
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "2";
+player.SetVar('o'+inp, numb);
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "3";
+player.SetVar('o'+inp, numb);
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "4";
+player.SetVar('o'+inp, numb);
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "5";
+player.SetVar('o'+inp, numb);
+}
+
+function Script7()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "6";
+player.SetVar('o'+inp, numb);
+}
+
+function Script8()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "7";
+player.SetVar('o'+inp, numb);
+}
+
+function Script9()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "8";
+player.SetVar('o'+inp, numb);
+}
+
+function Script10()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "9";
+player.SetVar('o'+inp, numb);
+}
+
+function Script11()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "0";
+player.SetVar('o'+inp, numb);
+}
+
+function Script12()
+{
+  var player = GetPlayer();
+var e1 = player.GetVar('o4_a');
+var e2 = e1.replace(/\s+/g, ''); 
+player.SetVar('o4_a',e2);
+}
+
+function Script13()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "1";
+player.SetVar('o'+inp, numb);
+}
+
+function Script14()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "2";
+player.SetVar('o'+inp, numb);
+}
+
+function Script15()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "3";
+player.SetVar('o'+inp, numb);
+}
+
+function Script16()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "4";
+player.SetVar('o'+inp, numb);
+}
+
+function Script17()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "5";
+player.SetVar('o'+inp, numb);
+}
+
+function Script18()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "6";
+player.SetVar('o'+inp, numb);
+}
+
+function Script19()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "7";
+player.SetVar('o'+inp, numb);
+}
+
+function Script20()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "8";
+player.SetVar('o'+inp, numb);
+}
+
+function Script21()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "9";
+player.SetVar('o'+inp, numb);
+}
+
+function Script22()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "0";
+player.SetVar('o'+inp, numb);
+}
+
+function Script23()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "1";
+player.SetVar('o'+inp, numb);
+}
+
+function Script24()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "2";
+player.SetVar('o'+inp, numb);
+}
+
+function Script25()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "3";
+player.SetVar('o'+inp, numb);
+}
+
+function Script26()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "4";
+player.SetVar('o'+inp, numb);
+}
+
+function Script27()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "5";
+player.SetVar('o'+inp, numb);
+}
+
+function Script28()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "6";
+player.SetVar('o'+inp, numb);
+}
+
+function Script29()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "7";
+player.SetVar('o'+inp, numb);
+}
+
+function Script30()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "8";
+player.SetVar('o'+inp, numb);
+}
+
+function Script31()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "9";
+player.SetVar('o'+inp, numb);
+}
+
+function Script32()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "0";
+player.SetVar('o'+inp, numb);
+}
+
+function Script33()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "1";
+player.SetVar('o'+inp, numb);
+}
+
+function Script34()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "2";
+player.SetVar('o'+inp, numb);
+}
+
+function Script35()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "3";
+player.SetVar('o'+inp, numb);
+}
+
+function Script36()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "4";
+player.SetVar('o'+inp, numb);
+}
+
+function Script37()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "5";
+player.SetVar('o'+inp, numb);
+}
+
+function Script38()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "6";
+player.SetVar('o'+inp, numb);
+}
+
+function Script39()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "7";
+player.SetVar('o'+inp, numb);
+}
+
+function Script40()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "8";
+player.SetVar('o'+inp, numb);
+}
+
+function Script41()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "9";
+player.SetVar('o'+inp, numb);
+}
+
+function Script42()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "0";
+player.SetVar('o'+inp, numb);
+}
+
+function Script43()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "1";
+player.SetVar('o'+inp, numb);
+}
+
+function Script44()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "2";
+player.SetVar('o'+inp, numb);
+}
+
+function Script45()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "3";
+player.SetVar('o'+inp, numb);
+}
+
+function Script46()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "4";
+player.SetVar('o'+inp, numb);
+}
+
+function Script47()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "5";
+player.SetVar('o'+inp, numb);
+}
+
+function Script48()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "6";
+player.SetVar('o'+inp, numb);
+}
+
+function Script49()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "7";
+player.SetVar('o'+inp, numb);
+}
+
+function Script50()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "8";
+player.SetVar('o'+inp, numb);
+}
+
+function Script51()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "9";
+player.SetVar('o'+inp, numb);
+}
+
+function Script52()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "0";
+player.SetVar('o'+inp, numb);
+}
+
+function Script53()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "1";
+player.SetVar('o'+inp, numb);
+}
+
+function Script54()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "2";
+player.SetVar('o'+inp, numb);
+}
+
+function Script55()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "3";
+player.SetVar('o'+inp, numb);
+}
+
+function Script56()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "4";
+player.SetVar('o'+inp, numb);
+}
+
+function Script57()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "5";
+player.SetVar('o'+inp, numb);
+}
+
+function Script58()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "6";
+player.SetVar('o'+inp, numb);
+}
+
+function Script59()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "7";
+player.SetVar('o'+inp, numb);
+}
+
+function Script60()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "8";
+player.SetVar('o'+inp, numb);
+}
+
+function Script61()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "9";
+player.SetVar('o'+inp, numb);
+}
+
+function Script62()
+{
+  var player = GetPlayer();
+var inp = player.GetVar('input');
+var numb = player.GetVar('o'+inp);
+
+numb = numb + "0";
+player.SetVar('o'+inp, numb);
+}
+
